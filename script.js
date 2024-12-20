f35870d8253067271f404f54a3410d14a69f34a4
VShef <volodymyr.shevchun96@gmail.com>
2024-12-19 22:22:25 -0600

    Update script.js

--- script.js
+++ script.js
@@ -30,13 +30,13 @@ document.addEventListener("DOMContentLoaded", () => {
     // Пример альтернативных сценариев. В будущем можно вынести в отдельный файл или дополнять при загрузке данных.
     // Ключ формата: "год - название события"
     const alternativesData = {
-        "476-Падение Римской Империи": [
+        "476-The Fall of the Roman Empire": [
             {
-                title: "Римская империя не распалась",
+                title: "The Roman Empire did not collapse",
                 color: "#aabbcc",
                 scenario: [
-                    { year: 500, text: "К 500 году империя процветала... Технологии, искусство, культура..." },
-                    { year: 800, text: "К 600 году Рим освоил новые территории и технологии..." }
+                    { year: 500, text: "By the year 500, the empire was thriving... Technology, art, culture..." },
+                    { year: 800, text: "By the year 800, Rome had mastered new territories and technologies..." }
                 ]
             },
             {
